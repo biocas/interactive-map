23c1745b32c4a9efafe1141f46c5519de629bb0f
PPandP <ppandp@PPandPs-iMac-2.local>
2019-07-08 12:12:32 +1000

    add nav and canvas functionality

--- main.js
+++ main.js
@@ -2,13 +2,26 @@ alert('hello world!');
 
 var canvas = document.getElementById('map-canvas');
 var ctx = canvas.getContext('2d');
-
 var img = new Image();
 
-img.onload = function(){
-canvas.width = img.naturalWidth
-canvas.height = img.naturalHeight
-ctx.drawImage(img, 0, 0);
+img.onload = start;
+img.src = 'img/map_large.png';
+
+function start() {
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height); 
 }
 
-img.src = 'img/map3.svg';
+//get DPI, canvas and context
+var dpi = window.devicePixelRatio;
+
+function fix_dpi() {
+//get CSS height
+//the + prefix casts it to an integer
+//the slice method gets rid of "px"
+let style_height = +getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
+//get CSS width
+let style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
+//scale the canvas
+canvas.setAttribute('height', style_height * dpi);
+canvas.setAttribute('width', style_width * dpi);
+}
