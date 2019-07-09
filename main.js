d7bdb1813af28b048951bb95dec80ea037159e79
PPandP <ppandp@PPandPs-iMac-2.local>
2019-07-09 13:38:16 +1000

    add ids to svg hotels and caravan parks markers

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
-/*var canvas = document.getElementById('map-canvas');
+window.addEventListener("load", function() {
+    var svgObject = document.getElementById('object').contentDocument; 
+    
+    var svg = svgObject.getElementById('Base_2')
+      console.log(svg);   
+    /* adds ids to all caravan parks markers */
+    var Cmarker = svgObject.getElementsByClassName('cls-63');
+    Cmarker[0].setAttributeNS(null, 'id', 'Caravan-Park');
+    Cmarker[1].setAttributeNS(null, 'id', 'rest-a-while');
+    Cmarker[2].setAttributeNS(null, 'id', 'barneys');
+    /* adds ids to all hotel markers */
+    var Hmarker = svgObject.getElementsByClassName('cls-61');
+    Hmarker[0].setAttributeNS(null, 'id','Royal'); 
+    Hmarker[1].setAttributeNS(null, 'id','Gilgandra');
+    Hmarker[2].setAttributeNS(null, 'id','Orana-Windmill');
+    Hmarker[3].setAttributeNS(null, 'id','Three-ways');
+    Hmarker[4].setAttributeNS(null, 'id','Bungalow');
+    Hmarker[5].setAttributeNS(null, 'id','Silver-oaks');
+    Hmarker[6].setAttributeNS(null, 'id','Cooee-Motel');
+    Hmarker[7].setAttributeNS(null, 'id','Motor-Inn');
+    Hmarker[8].setAttributeNS(null, 'id','Railway-Hotel');
+    Hmarker[9].setAttributeNS(null, 'id','Lodge');
+    
+    });
+
+   
+
+/*
+CANVAS FUNCTIONALITY SETUP - HERE JUST IN CASE WE NEED IT BACK
+
+
+var canvas = document.getElementById('map-canvas');
 var ctx = canvas.getContext('2d');
 var img = new Image();
 
